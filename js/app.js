71be310f2a68451fcbdd4598d457e2371abb6983
MichalJozwa <michal.awzoj@gmail.com>
2021-12-12 18:36:56 +0100

    sections done & cards js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 console.log("test works well!🎯");
+const cards = document.querySelectorAll('.extended-card');
+
+function toggleActive(){
+   if(!this.classList.contains('active')){
+      cards.forEach((element) => {
+         element.classList.remove('active');
+      });
+      this.classList.add('active');
+   } else{this.classList.remove('active');}
+}
+
+cards.forEach((item) => {
+   item.addEventListener('click', toggleActive);
+})
