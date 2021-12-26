491349b2ccbb9b6e9e063a4113ed473ca8077072
MichalJozwa <michal.awzoj@gmail.com>
2021-12-26 03:19:58 +0100

    more clearly files

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
-console.log("test works well!🎯");
+// window.addEventListener("scroll", function(event){
+//    var top = this.scrollY;
+//    if(top >= 700 && top < 800){
+//       document.querySelector('.extended-card:first-of-type').classList.add('active');
+//       document.querySelector('.extended-card:nth-child(2)').classList.remove('active');
+//    } else if(top >= 800){
+//       document.querySelector('.extended-card:first-of-type').classList.remove('active');
+//       document.querySelector('.extended-card:nth-child(2)').classList.add('active');
+//    } else if(top >= 900){
+//       document.querySelector('.extended-card:nth-child(2)').classList.remove('active');
+//       document.querySelector('.extended-card:nth-child(3)').classList.add('active');
+//    }
+//    if(top < 700){
+//       document.querySelectorAll('.extended-card').classList.remove('active');
+//    }
+// }, false);
+
 const cards = document.querySelectorAll('.extended-card');
 
 function toggleActive(){
